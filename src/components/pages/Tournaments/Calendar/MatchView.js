4d8664cdb86185e61d910399297fcacddce96cbf
L4wl3sS <l4wl3ss1989@gmail.com>
2020-10-16 10:07:19 +0200

    added isMatchPlaying status=3  visual adapatation

--- src/components/pages/Tournaments/Calendar/MatchView.js
+++ src/components/pages/Tournaments/Calendar/MatchView.js
@@ -1,86 +1,79 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { getFormattedDateTime } from "../../../common/FormsMobx/Utils";
-import { getUploadsImg } from "../../../helpers/Utils";
-import Loc from "../../../common/Locale/Loc";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { getFormattedDateTime } from '../../../common/FormsMobx/Utils';
+import { getUploadsImg } from '../../../helpers/Utils';
+import Loc from '../../../common/Locale/Loc';
 
-const matchHasResult = (match) => {
+const matchHasResult = match => {
   const s = match.status;
   // eslint-disable-next-line eqeqeq
   return s == 3 || s == 4;
 };
 
 class MatchView extends Component {
-  getMatchSanctionStatusBadge = (match) => {
-    return (
-      <span
-        className={
-          match.status <= 3 ? "SanctionMatchPending" : "SanctionMatchDone"
-        }
-      ></span>
-    );
+  getMatchSanctionStatusBadge = match => {
+    return <span className={match.status <= 3 ? 'SanctionMatchPending' : 'SanctionMatchDone'}></span>;
   };
 
   render() {
     const p = this.props;
     const { match, home, visitor, tId } = p;
+    const isMatchPlaying = match.status === 3;
 
     return (
       <tr key={match.id}>
         <td className="MatchSchedule">
-          <Link to={"/tournaments/" + tId + "/matches/" + match.id}>
+          <Link to={'/tournaments/' + tId + '/matches/' + match.id}>
             {/* <span className='Day'>{day.name}</span> */}
-            {p.displaySanctioned
-              ? this.getMatchSanctionStatusBadge(match)
-              : null}
-            <span className="Date">
-              {getFormattedDateTime(new Date(match.startTime))}
-            </span>
+            {p.displaySanctioned ? this.getMatchSanctionStatusBadge(match) : null}
+            <span className="Date">{getFormattedDateTime(new Date(match.startTime))}</span>
           </Link>
         </td>
         <td className="MatchTeamName Home ">
           {home ? (
-            <Link to={"/tournaments/" + tId + "/teams/" + home.id}>
-              {home.name}
-            </Link>
+            <Link to={'/tournaments/' + tId + '/teams/' + home.id}>{home.name}</Link>
           ) : (
             <span className="MatchTeamDesc">{match.homeTeamDescription}</span>
           )}
         </td>
         <td className="MatchTeamLogo Home">
-          {home
-            ? getUploadsImg(home.logoImgUrl, home.id, "team", "TeamLogo")
-            : null}
+          {home ? getUploadsImg(home.logoImgUrl, home.id, 'team', 'TeamLogo') : null}
         </td>
 
         {matchHasResult(match) ? (
           <td className="MatchScore">
-            <span className="Score">{match.homeScore}</span>{" "}
-            <span className="Score">{match.visitorScore}</span>
+            {isMatchPlaying ? (
+              <div className="ScoreWrapper Playing">
+                <span className="MatchPlaying">
+                  <Loc>MatchStatus3</Loc>
+                </span>
+                <span className="Score">{match.homeScore}</span>
+                <span className="Score">{match.visitorScore}</span>
+              </div>
+            ) : (
+              <React.Fragment>
+                <span className="Score">{match.homeScore}</span>
+                <span className="Score">{match.visitorScore}</span>
+              </React.Fragment>
+            )}
           </td>
         ) : (
           <td className="MatchNoScore">
-            <Loc>{"MatchStatus" + match.status}</Loc>
+            <Loc>{'MatchStatus' + match.status}</Loc>
           </td>
         )}
 
         <td className="MatchTeamLogo Visitor">
-          {visitor
-            ? getUploadsImg(visitor.logoImgUrl, visitor.id, "team", "TeamLogo")
-            : null}
+          {visitor ? getUploadsImg(visitor.logoImgUrl, visitor.id, 'team', 'TeamLogo') : null}
         </td>
         <td className="MatchTeamName Visitor">
           {visitor ? (
-            <Link to={"/tournaments/" + tId + "/teams/" + visitor.id}>
-              {visitor.name}
-            </Link>
+            <Link to={'/tournaments/' + tId + '/teams/' + visitor.id}>{visitor.name}</Link>
           ) : (
-            <span className="MatchTeamDesc">
-              {match.visitorTeamDescription}
-            </span>
+            <span className="MatchTeamDesc">{match.visitorTeamDescription}</span>
           )}
         </td>
-        <td>{match.field ? "@ " + (match.field && match.field.name) : null}</td>
+        <td>{match.field ? '@ ' + (match.field && match.field.name) : null}</td>
       </tr>
     );
   }
